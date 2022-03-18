883aac00eed368f9bf3035b16b8b9b480684793c
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-03-18 08:25:02 +0100

    started on the fetch

--- src/index.js
+++ src/index.js
@@ -1,54 +1,157 @@
-import "./style.css"
-import "bootstrap/dist/css/bootstrap.css"
-import * as bootstrap from 'bootstrap';
-import '@popperjs/core';
-import "./jokeFacade"
-import jokeFacade from "./jokeFacade"
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
+import * as bootstrap from "bootstrap";
+import "@popperjs/core";
 
-document.getElementById("all-content").style.display = "block"
+let sections = document.querySelectorAll("#sections section");
+let uls = document.querySelectorAll("#uls li");
 
-/* 
-  Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
-*/
-
-/* JS For Exercise-1 below */
-
-
-/* JS For Exercise-2 below */
-
-
-
-/* JS For Exercise-3 below */
-
-
-/* 
-Do NOT focus on the code below, UNLESS you want to use this code for something different than
-the Period2-week2-day3 Exercises
-*/
-
-function hideAllShowOne(idToShow)
-{
-  document.getElementById("about_html").style = "display:none"
-  document.getElementById("ex1_html").style = "display:none"
-  document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("ex3_html").style = "display:none"
-  document.getElementById(idToShow).style = "display:block"
+hideAll();
+for (let i = 0; i < sections.length; i++) {
+  uls[i].addEventListener("click", () => {
+    document.querySelector("#hide").style.display = "none";
+    hideAll();
+    sections[i].style.display = "block";
+    uls[i].classList.add("selected");
+    check(i);
+  });
 }
 
-function menuItemClicked(evt)
-{
-  const id = evt.target.id;
-  switch (id)
-  {
-    case "ex1": hideAllShowOne("ex1_html"); break
-    case "ex2": hideAllShowOne("ex2_html"); break
-    case "ex3": hideAllShowOne("ex3_html"); break
-    default: hideAllShowOne("about_html"); break
+function hideAll() {
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.display = "none";
+    uls[i].classList.remove("selected");
   }
-  evt.preventDefault();
 }
-document.getElementById("menu").onclick = menuItemClicked;
-hideAllShowOne("about_html");
 
+let idBtn = document.querySelector("#getByIdBtn");
+let error = document.querySelector("#errorText");
+idBtn.addEventListener("click", () => {
+  let inputValue = document.querySelector("#idPerson");
+  if (inputValue.value == "") {
+    error.innerHTML = "You have to pick a number";
+  } else {
+    getUserById(inputValue.value);
+  }
+});
 
+let hobbyBtn = document.querySelector("#getByHobbyBtn");
+let hobbyError = document.querySelector("#errorTextHobby");
+hobbyBtn.addEventListener("click", () => {
+  console.log("here btn");
+  let inputValue = document.querySelector("#hobby");
+  if (inputValue.value == "") {
+    hobbyError.innerHTML = "You have to pick a hobby";
+  } else {
+    getUserByHobby(inputValue.value);
+  }
+});
 
+function check(i) {
+  switch (i) {
+    case 0:
+      // Get all users
+      getAllUsers();
+      break;
+    case 1:
+      // Get user by id
+      break;
+    case 2:
+      // Delete user
+      break;
+    case 3:
+      // Get users by zipcode
+      break;
+    case 4:
+      // Get users by hobby
+      getAllHobbies();
+      break;
+    default:
+    // code block
+  }
+}
+
+function getAllUsers() {
+  fetch("http://localhost:8080/devops_starter_war_exploded/api/users/all", {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      result.forEach((element) => {
+        sections[0].innerHTML += `<p>User id: ${element.idPerson}, name: ${element.firstName} ${element.lastName}, phone number: ${element.phoneNumber}, email: ${element.email}</p>`;
+      });
+    });
+}
+
+function getUserById(id) {
+  fetch("http://localhost:8080/devops_starter_war_exploded/api/users/" + id, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      error.innerHTML = `<p>User id: ${result.idPerson}, name: ${result.firstName} ${result.lastName}, phone number: ${result.phoneNumber}, email: ${result.email}</p>`;
+    });
+}
+
+function getAllHobbies() {
+  fetch("http://localhost:8080/devops_starter_war_exploded/api/hobby/all", {
+    //method: "GET", // *GET, POST, PUT, DELETE, etc.
+    //mode: "cors", // no-cors, *cors, same-origin
+    //cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      result.forEach((element) => {
+        sections[4].innerHTML += `<p>Hoby id: ${element.id}, name: ${element.name}, wiki link: ${element.wikiLink}, category: ${element.category}, type: ${element.type}</p>`;
+      });
+    });
+}
+function getUserByHobby(hobby) {
+  console.log("here hh");
+  fetch(
+    "http://localhost:8080/devops_starter_war_exploded/api/hobby/" + hobby,
+    {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      if (result == null) {
+        hobbyError.innerHTML += `None of our users has that hobby`;
+      } else {
+        result.forEach((element) => {
+          hobbyError.innerHTML += `<p>User id: ${element.idPerson}, name: ${element.firstName} ${element.lastName}, phone number: ${element.phoneNumber}, email: ${element.email}</p>`;
+        });
+      }
+    });
+}
